470e8130f891cfb6a765bc565da7062633bf0c73
MIRAW <145260048+mirawbtw@users.noreply.github.com>
2025-02-23 17:45:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const path = require("path");
 const cors = require("cors");
 const { authenticateToken, isAdmin } = require("./middlewares/authMiddleware");
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
